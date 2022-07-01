add5443025e3d15567e2c7a7774e7189131a7913
Hormaza <matias@hormaza.com.ar>
2022-07-01 17:45:27 +0200

    Change behavior saving csv file as text

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,24 +11,50 @@ function Home() {
     // Flag to know if it is a png file type
     const isPNG = !!inputFile.type.match("png");
 
-    const reader = new FileReader();
-    reader.readAsDataURL(inputFile);
+    if (isPNG) {
+      saveImage(inputFile);
+    } else {
+      saveSheet(inputFile);
+    }
 
-    // Saving in localstorage when file is load in memory
-    reader.addEventListener("load", function (e) {
-      const newFile = e.target.result;
-      // Setting the key of localstorage up to filetype
-      const localStorageKey = isPNG ? "images" : "sheets";
+    function saveImage(file) {
+      const reader = new FileReader();
+      // read as URL to save in localStorage
+      reader.readAsDataURL(file);
+
+      // Saving in localstorage when file is load in memory
+      reader.addEventListener("load", function (e) {
+        const newImage = e.target.result;
+        // Setting the key of localstorage
+        const localStorageKey = "images";
+        saveOnLocalStorage(localStorageKey, newImage);
+      });
+    }
+
+    function saveSheet(file) {
+      const reader = new FileReader();
+      // read as text to save in localStorage
+      reader.readAsText(file);
+
+      reader.addEventListener("load", function (e) {
+        const localStorageKey = "sheets";
+        const newSheet = e.target.result;
+        saveOnLocalStorage(localStorageKey, newSheet);
+      });
+      setIsFileLoaded(true);
+    }
+
+    function saveOnLocalStorage(key, value) {
       // Getting the current value. If doesn't exist create a empty array string
       // * localStorage only can save strings
-      const currentValue = localStorage.getItem(localStorageKey) || "[]";
+      const currentValue = localStorage.getItem(key) || "[]";
       // Parsing string
       const parsedValue = JSON.parse(currentValue);
-      const images = [...parsedValue, newFile];
+      const newList = [...parsedValue, value];
       // Saving in localStorage
-      localStorage.setItem(localStorageKey, JSON.stringify(images));
+      localStorage.setItem(key, JSON.stringify(newList));
       setIsFileLoaded(true);
-    });
+    }
   };
 
   return (
