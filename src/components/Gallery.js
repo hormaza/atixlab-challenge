8d56683c60baddf620d6f0145c8c23592185520d
Hormaza <matias@hormaza.com.ar>
2022-07-01 23:28:35 +0200

    Add Gallery styles

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,15 +1,47 @@
 import React from "react";
 
 function Gallery() {
+  const [showModal, setShowModal] = React.useState(false);
+  const [modalImgSource, setModalImgSource] = React.useState("");
   const images = JSON.parse(localStorage.getItem("images") || "[]");
+
+  function displayImage(imgSrc) {
+    setShowModal(true);
+    setModalImgSource(imgSrc);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
   return (
-    <div>
-      {!!images.length ? (
-        images.map((img, i) => (
-          <img alt={"Image number " + i} key={"image_" + i} src={img} />
-        ))
-      ) : (
-        <h2>No images found in gallery.</h2>
+    <div id="Gallery">
+      <h2>Gallery Section</h2>
+      <div style={{ display: "flex" }}>
+        {!!images.length ? (
+          <div className="grid">
+            {images.map((img, i) => (
+              <div
+                className="image"
+                key={"image_" + i}
+                style={{ backgroundImage: `url(${img})` }}
+                onClick={() => displayImage(img)}
+              ></div>
+            ))}
+          </div>
+        ) : (
+          <h3>No images found in gallery.</h3>
+        )}
+      </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <div className="closeButton" onClick={closeModal}>
+              x
+            </div>
+            <img id="modalImg" width="100%" src={modalImgSource} />
+          </div>
+        </div>
       )}
     </div>
   );
