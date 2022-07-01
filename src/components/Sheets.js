3c85af43195f830253d7bb5eae4891d80616b88c
Hormaza <matias@hormaza.com.ar>
2022-07-01 19:43:45 +0200

    Add expand and collapse animation to sheet files

--- src/components/Sheets.js
+++ src/components/Sheets.js
@@ -15,6 +15,18 @@ function Sheets() {
     // construct object
     return { header: headerValues, body: bodyValues };
   }
+
+  function toggleTable(index) {
+    const element = document.querySelectorAll(".table-container")[index];
+    const isExpanded = element.classList.contains("expanded");
+    if (isExpanded) {
+      element.classList.remove("expanded");
+      element.classList.add("collapsed");
+    } else {
+      element.classList.add("expanded");
+      element.classList.remove("collapsed");
+    }
+  }
   return (
     <div id="Sheets">
       <h2>Sheets Section</h2>
@@ -22,24 +34,29 @@ function Sheets() {
         {!!sheets.length ? (
           sheets.map(parseCSV).map((csv, i) => {
             return (
-              <table key={"sheet_" + i}>
-                <thead>
-                  <tr>
-                    {csv.header.map((value, i) => (
-                      <th key={value + i}>{value}</th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {csv.body.map((row, i) => (
-                    <tr key={"row_" + i}>
-                      {row.map((value, i) => (
-                        <td key={value + i}>{value}</td>
+              <div key={"sheet_" + i}>
+                <button onClick={() => toggleTable(i)}>CSV {i + 1}</button>
+                <div className="table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        {csv.header.map((value, i) => (
+                          <th key={value + i}>{value}</th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {csv.body.map((row, i) => (
+                        <tr key={"row_" + i}>
+                          {row.map((value, i) => (
+                            <td key={value + i}>{value}</td>
+                          ))}
+                        </tr>
                       ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             );
           })
         ) : (
